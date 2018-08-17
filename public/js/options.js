3a340feb1c5b338312e609120ffe9b0881f64d47
Wilson Su <wilson_su@trend.com.tw>
2018-08-17 14:58:06 +0800

    add 'options' service

---
+++ public/js/options.js
@@ -0,0 +1,14 @@
+define(["util"], function(util){
+  return {
+    brains () {
+      const brains = util.search(/brains=(-?\d,\d,\d,\d)/, 1);
+      return brains ? brains.split(',').map(Number) : null;
+    },
+    cheat () {
+      return util.search(/cheat=(yes)/, 1) === 'yes';
+    },
+    replay () {
+      return util.search(/replay=(\d+)/, 1) || -1;
+    },
+  };
+});
